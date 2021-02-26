c3caa1dad1a87ec9b257ead1d412010797abb8e5
Bruno Mignoni <bruno.mignoni-extern@rcibanque.com>
2021-02-26 13:25:07 -0300

    Commit inicial

---
+++ teste.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const port = 8080;
+
+app.use(bodyParser.json());
+app.use(bodyParser.text());
+app.use(bodyParser.raw());
+app.use(bodyParser.urlencoded({ extended: false, type: '*/*' }));
+
+app.all('/*', (req, res) => {
+  console.log('req: '+req.originalUrl);
+
+
+  console.log('body: '+req.body);
+  res.setHeader("headeDeRetorno", Date.now());
+
+
+//iterating dinamically
+let props = [];
+for(let prop in req){
+if(prop.substring(0, 1)!="_"){
+	props.push(prop);
+}
+
+}
+
+  res.json(JSON.stringify(req, props));
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
